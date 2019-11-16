0ab0df7e053e99a8492dabaa69bdc9e30a15b4e3
hycpower1221 <hycpower1221@gmail.com>
2019-11-16 13:42:08 +0300

    а вот и pnk подъехала

--- lab4/lab4/Form1.cs
+++ lab4/lab4/Form1.cs
@@ -17,7 +17,7 @@ namespace lab4
             InitializeComponent();
         }
 
-        int k = 24, n, p, dmin = 3, x, y;
+        int k = 5, n, p, dmin = 3, x, y;
         double[,] pnk, hp;
         double[] a, b, s;
         Random rand = new Random();
@@ -40,6 +40,8 @@ namespace lab4
             x = p;
             int pp = 0;
             y = Convert.ToInt32(Math.Pow(2, p));
+            pnk = new double[k, n];
+            for (int i = 0; i < k; i++) pnk[i, i] = 1;
             double[,] prov = new double[y, x + 2];
             for (int i = 0; i < y; i++)
             {
@@ -52,24 +54,25 @@ namespace lab4
                     
                 }
             }
-            for (int i = 0; i < y; i++)
-            {
-                for (int j = 0; j < x + 1; j++)
-                {
-                    richTextBox1.AppendText(prov[i, j].ToString());
-                }
-                richTextBox1.AppendText("\n");
-            }
             for (int i = 0; i < k; i++)
             {
                 int h = rand.Next(y);
                 if (prov[h, x] >= 2 && prov[h, x + 1] == 0)
                 {
-                    for (int j = 0; j < x + 2; j++) richTextBox2.AppendText(prov[h, j].ToString());
-                    richTextBox2.AppendText("\n");
+                    int g = 0;
+                    for (int j = k; j < n; j++)
+                    {
+                        pnk[i, j] = prov[h, g];
+                        g++;
+                    }
                     prov[h, x + 1] = 1;
                 }
                 else i--;
+            }
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = 0; j < n; j++) richTextBox1.AppendText(pnk[i,j].ToString());
+                richTextBox1.AppendText("\n");
             }
         }
 
